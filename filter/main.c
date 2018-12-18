f07026ac23c0405b3e09741f3db24bc8e08f57f3
Jeremy Mattfeld <jmattfeld@scanivalve.com>
2018-12-17 16:01:39 -0800

    filter is working

--- filter/main.c
+++ filter/main.c
@@ -1,19 +1,42 @@
 // main.c
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define SIMULATED_DATA_PTS 3220
+#define NUM_CHANNELS 64
 #define INPUT_SAMPLE_LEN 1
-#define FILTER_LEN 24
+#define FILTER_LEN 10
 #define INPUT_BUFFER_LEN FILTER_LEN - 1 + INPUT_SAMPLE_LEN
+#define INFILE "unfiltered.csv"
+#define OUTPUT_PATH "C:\\Users\\Jeremy.SV\\Documents\\octave-projects\\"
+#define OUTFILE "filtered.csv"
 
-float sampleBuffer[INPUT_BUFFER_LEN];
-float unfiltered[3220];
-float filtered[3220];
+float sampleBuffer[NUM_CHANNELS][INPUT_BUFFER_LEN];
 
-double coeffs[FILTER_LEN] = { 0.0061539,0.0075251,0.0114762,0.0177629,0.0259528,0.0354573,
-							0.0455770,0.0555560,0.0646414,0.0721428,0.0774877,0.0802668,
-							0.0802668,0.0774877,0.0721428,0.0646414,0.0555560,0.0455770,
-							0.0354573,0.0259528,0.0177629,0.0114762,0.0075251,0.0061539 };
+// simulated in/out streams
+float unfiltered[SIMULATED_DATA_PTS];
+float filtered[SIMULATED_DATA_PTS];
 
+// FILTER_LEN 10 / cutoff freq=0.010417 Hz
+double coeffs[FILTER_LEN] = { 0.016154,0.037930,0.093105,0.155901,0.196910,
+							  0.196910,0.155901,0.093105,0.037930,0.016154 };
+
+// FLITER_LEN 16 / cutoff freq=0.015625 Hz
+//double coeffs[FILTER_LEN] = { 0.0096199,0.0144736,0.0281796,0.0484536,
+//							  0.0718235,0.0942400,0.1117946,0.1214152,
+//							  0.1214152,0.1117946,0.0942400,0.0718235,
+//							  0.0484536,0.0281796,0.0144736,0.0096199 };
+
+// FILTER_LEN 24 / cutoff freq=0.015625 Hz
+//double coeffs[FILTER_LEN] = { 0.0061539,0.0075251,0.0114762,0.0177629,0.0259528,0.0354573,
+//							0.0455770,0.0555560,0.0646414,0.0721428,0.0774877,0.0802668,
+//							0.0802668,0.0774877,0.0721428,0.0646414,0.0555560,0.0455770,
+//							0.0354573,0.0259528,0.0177629,0.0114762,0.0075251,0.0061539 };
+
+// use to initialize input buffer to current temp if desired
 int signalInit(float tempin, float *pSignal, int len)
 {
 	int i;
@@ -30,40 +53,98 @@ int signalInit(float tempin, float *pSignal, int len)
 
 float LowPassFilter(int chan, float tempin)
 {
-	float acc;
-	return 0.000;
-	int k;	// coeff idx
+	float acc;	// accumulator
+	int k;		// coeff idx
 	static unsigned idx = 0;
 	int n = idx % INPUT_BUFFER_LEN;
 
-	// write new sample to the location of the oldest sample, start at 0
-	sampleBuffer[n] = tempin;
+	// write new sample to the index of the oldest sample, start at 0
+	sampleBuffer[chan - 1][n] = tempin;
 
 	// apply filter to the input sample
 	acc = 0;
 	for (k = 0; k < FILTER_LEN; k++)
 	{
-		acc += (float)coeffs[k] * sampleBuffer[(n + INPUT_BUFFER_LEN - k) % INPUT_BUFFER_LEN];
+		acc += (float)coeffs[k] * sampleBuffer[chan - 1][(n + INPUT_BUFFER_LEN - k) % INPUT_BUFFER_LEN];
 	}
+
+	// move the idnex into the input buffer for next function call
 	idx++;
 	return acc;
+}
+
+int ReadLine(FILE *fp, char * tstr) 
+{
+	int c, i, bEof;
+
+	i = 0;
+	bEof = 0;
+	while (1) {
+		c = fgetc(fp);
+		if (c == EOF) {
+			// End of file encountered
+			tstr[i] = 0;
+			bEof = 1;
+			break;
+		}
+		else if ((c == '\r') || (c == '\n')) {
+			// End of line
+			tstr[i] = 0;
+			i = 0;
+			break;
+		}
+		else {
+			// Build the string, dont return
+			tstr[i] = (char)c;
+			i++;
+		}
+	}
+	return(bEof);
 }
 
 int main()
 {
 	int i, len;
+	FILE *infp, *outfp;
+	char tstr[24];
+	const char * outfilename = OUTPUT_PATH OUTFILE;
 
 	// open input file
+	printf("Opening %s\n", INFILE);
+	infp = fopen(INFILE, "r");
+	if (NULL == infp)
+		return -1;
+
+	// open output file
+	printf("Opening %s\n", OUTFILE);
+	outfp = fopen(outfilename, "w");
+	if (NULL == outfp)
+		return -1;
+
+	// read unfiltered data into array
+	printf("Reading unfiltered data into array\n");
+	i = 0;
+	while (1)
+	{
+		if (ReadLine(infp, tstr))
+			break;
+		unfiltered[i] = strtof(tstr, NULL);
+		i++;
+	}
 
 	// initialize signal array
-	len = sizeof sampleBuffer / sizeof(float);
-	signalInit(0, sampleBuffer, len);
-	//printf("length of signal: %d\n", sizeof inSamp / sizeof(float));
-	
-	for (i = 0; i < len; i++)
+	len = sizeof sampleBuffer[0] / sizeof(float);
+	signalInit(0, sampleBuffer[0], len);
+
+	// apply filter (using single channel buffer)
+	for (i = 0; i < SIMULATED_DATA_PTS; i++)
 	{
-		printf("%.3f\n", sampleBuffer[i]);
+		filtered[i] = LowPassFilter(1, unfiltered[i]);
+		fprintf(outfp, "%.3f\n", filtered[i]);
 	}
+
+	fclose(infp);
+	fclose(outfp);
 
 	return 0;
 }
